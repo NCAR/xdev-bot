07053ce8e2947e0481ad48af01489e5ec854842a
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-02-20 19:24:04 -0700

    Add pull request event

--- xdev_bot/__main__.py
+++ xdev_bot/__main__.py
@@ -28,6 +28,26 @@ async def issue_opened_event(event, gh, *args, **kwargs):
     await gh.post(url, data={"body": message})
 
 
+
+@router.register("pull_request", action="closed")
+async def pull_request_merge_event(event, gh, *args, **kwargs):
+    """ Whenever a PR is merged, greet the author and say thanks to both the PR author 
+    and the person who merged it.
+    
+    """
+
+    url = event.data["pull_request"]["comments_url"]
+    
+    # PR can be closed without being merged. 
+    merged = event.data["pull_request"]["merged"]
+
+    if merged:
+        author = event.data["pull_request"]["user"]["login"]
+        merged_by = event.data["pull_request"]["merged_by"]
+        message = f"Thank for your contribution @{author}! Thank you @{merged_by} for your merging this ! ( I am a bot)."
+        await gh.post(url, data={"body": message})
+
+
 async def main(request):
     # Read the GitHub webhook payload
     body = await request.read()
