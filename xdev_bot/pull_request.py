03a86b2d16c7901906fd0184eed31f1ffa2a35f6
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-02-21 12:33:17 -0700

    Whenever a PR is opened, create a card in `in-progress` column of `Backlog Queue`

--- xdev_bot/pull_request.py
+++ xdev_bot/pull_request.py
@@ -1,23 +1,31 @@
 """ Pull requests events """
 import gidgethub.routing
 
+from .project_board import PROJECT_BOARD
+
 router = gidgethub.routing.Router()
 
 
-@router.register("pull_request", action="closed")
-async def pull_request_merge_event(event, gh, *args, **kwargs):
-    """ Whenever a PR is merged, greet the author and say thanks to both the PR author
-    and the person who merged it.
+@router.register("pull_request", action="opened")
+async def pull_request_opened_event(event, gh, *args, **kwargs):
+    """ Whenever a PR is opened, create a card in:
+
+    - project_board = "Backlog Queue"
+    - column = "In Progress"
 
     """
-
+    in_progress_column_id = PROJECT_BOARD["columns"]["in_progress"]["id"]
+    project_board_name = PROJECT_BOARD["name"]
+    pull_request_url = event.data["pull_request"]["html_url"]
+    url = f"/projects/columns/{in_progress_column_id}/cards"
+    print(
+        f"Creating Card in {project_board_name} project board for pull request : {pull_request_url}"
+    )
     url = event.data["pull_request"]["comments_url"]
 
-    # PR can be closed without being merged.
-    merged = event.data["pull_request"]["merged"]
-
-    if merged:
-        author = event.data["pull_request"]["user"]["login"]
-        merged_by = event.data["pull_request"]["merged_by"]["login"]
-        message = f"Thank for your contribution @{author}! Thank you @{merged_by} for your merging this ! ( I am a :octocat: bot )."
-        await gh.post(url, data={"body": message})
+    # POST /projects/columns/:column_id/cards
+    await gh.post(
+        url,
+        data={"note": pull_request_url},
+        accept="application/vnd.github.inertia-preview+json",
+    )
