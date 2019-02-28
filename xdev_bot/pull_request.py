c46ab7c476b2f1fa2b8250f192a61a3eaa7fcfc9
Julia Kent <jkent@ucar.edu>
2019-02-28 12:33:35 -0700

    Copying assignee method from project_board

--- xdev_bot/pull_request.py
+++ xdev_bot/pull_request.py
@@ -42,11 +42,30 @@ async def pull_request_opened_event(event, gh, *args, **kwargs):
 
     # Mark new PRs as needing a review
     # POST /repos/:owner/:repo/issues/:number/labels
-
     await gh.post(issue_url, data={'labels': ['needs-review']})
 
     # Assigning PR author
-    await gh.post(pull_request_url, data={'assignees': list(author)})
+        note_items = card_note.split("/")
+
+    # This returns ['https:', '', 'github.com', 'org_or_user', 'repo_name', 'issues_or_pull', 'number']
+    # if note is a html_url to an issue or pull request, len(note_items) == 7
+    if len(note_items) == 7:
+        _event_type = note_items[-2]
+
+        # Construct Issue or PR API url
+        # * Issue url looks like https://api.github.com/repos/NCAR/xdev-bot-testing/issues/2
+        # * PR url looks like 'https://api.github.com/repos/NCAR/xdev-bot-testing/pulls/3'
+        if _event_type == "issues":
+            prefix = "/".join(note_items[-4:])
+
+        elif _event_type == "pull":
+            # https://developer.github.com/v3/pulls/#labels-assignees-and-milestones
+            # Every pull request is an issue, but not every issue is a pull request.
+            p = note_items[-4:]
+            p[-2] = "issues"
+            prefix = "/".join(p)
+    _event_api_url = "https://api.github.com/repos/" + prefix
+    await gh.post( _event_api_url, data={'assignees': list(author)})
 
 
 @router.register('pull_request', action='closed')
