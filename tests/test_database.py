64dd43afa2ea6cbe514752595d2f31773c09a632
Kevin Paul <kpaul@ucar.edu>
2019-03-26 11:17:48 -0600

    Adding S3 database functionality

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,4 +1,10 @@
-from xdev_bot.database import CardDB
+import s3fs
+import pytest
+import pandas as pd
+
+from xdev_bot.database import CardDB, S3CardDB
+
+S3 = s3fs.S3FileSystem(anon=False)
 
 
 def test_init_card_db():
@@ -8,9 +14,9 @@ def test_init_card_db():
 
 def test_append_new_card():
     cards = CardDB()
-    card_info = {'a': 1, 'b': 'c', 'd': 4.5}
-    cards.append(**card_info)
-    assert cards[0] == card_info
+    card0 = {'a': 1, 'b': 'c', 'd': 4.5}
+    cards.append(**card0)
+    assert cards[0] == card0
 
 
 def test_get_card_info_by_value():
@@ -23,3 +29,29 @@ def test_get_card_info_by_value():
     cards.append(**card2)
     assert cards.where(b='c') == [card0, card2]
     assert cards.where(b='c', a=1) == [card0]
+    assert len(cards) == 3
+
+
+@pytest.fixture
+def s3fn():
+    fn = 'xdev-bot/test_database.csv'
+    if S3.exists(fn):
+        S3.rm(fn)
+    yield fn
+    if S3.exists(fn):
+        S3.rm(fn)
+
+
+def test_init_s3_card_db(s3fn):
+    cards = S3CardDB(s3fn)
+    card0 = {'a': 1, 'b': 'c', 'd': 4.5}
+    cards.append(**card0)
+    card1 = {'a': 2, 'b': 'd', 'd': 5.6}
+    cards.append(**card1)
+    card2 = {'a': 3, 'b': 'c', 'd': 7.3}
+    cards.append(**card2)
+    assert len(cards) == 3
+    assert S3.exists(s3fn)
+    with S3.open(s3fn, 'r') as f:
+        df = pd.read_csv(f)
+    assert cards.dataframe.equals(df)
--- xdev_bot/database.py
+++ xdev_bot/database.py
@@ -1,3 +1,4 @@
+import s3fs
 import pandas as pd
 
 
@@ -18,5 +19,33 @@ class CardDB(object):
             df = df.loc[df[k] == kwds[k]]
         return df.to_dict('records')
 
+    def __len__(self):
+        return len(self._df)
 
-PROJECT_CARDS = CardDB()
+    @property
+    def dataframe(self):
+        return self._df
+
+
+class S3CardDB(CardDB):
+
+    def __init__(self, filename):
+        super(S3CardDB, self).__init__()
+        self._fn = filename
+        self._s3 = s3fs.S3FileSystem(anon=False)
+        try:
+            if self._s3.exists(self._fn):
+                with self._s3.open(self._fn, 'r') as f:
+                    self._df = pd.read_csv(f)
+            else:
+                self._df = pd.DataFrame()
+        except Exception:
+            raise
+
+    def append(self, **props):
+        super().append(**props)
+        with self._s3.open(self._fn, 'w') as f:
+            self._df.to_csv(f, index=False)
+
+
+PROJECT_CARDS = S3CardDB('xdev-bot/test_database.csv')
