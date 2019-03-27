e02ce667749d818da6741cc0407755db342fd994
Kevin Paul <kpaul@ucar.edu>
2019-03-27 08:00:53 -0600

    Add accept and kwargs convenience parameter

--- tests/test_gidgethub.py
+++ tests/test_gidgethub.py
@@ -15,9 +15,40 @@ def test_init_set_data():
     assert ghargs.data == data
 
 
+def test_init_set_accept():
+    url = 'https://github.com/NCAR/xdev-bot'
+    data = {'note': 'https://github.com/NCAR/xdev-bot/issues/11111'}
+    accept = 'application/vnd.github.inertia-preview+json'
+    ghargs = GHArgs(url, data=data, accept=accept)
+    assert ghargs.url == url
+    assert ghargs.data == data
+    assert ghargs.accept == accept
+
+
+def test_kwargs_with_accept():
+    url = 'https://github.com/NCAR/xdev-bot'
+    data = {'note': 'https://github.com/NCAR/xdev-bot/issues/11111'}
+    accept = 'application/vnd.github.inertia-preview+json'
+    ghargs = GHArgs(url, data=data, accept=accept)
+    assert ghargs.url == url
+    assert ghargs.data == data
+    assert ghargs.accept == accept
+    assert ghargs.kwargs == {'data': data, 'accept': accept}
+
+
+def test_kwargs_without_accept():
+    url = 'https://github.com/NCAR/xdev-bot'
+    data = {'note': 'https://github.com/NCAR/xdev-bot/issues/11111'}
+    ghargs = GHArgs(url, data=data)
+    assert ghargs.url == url
+    assert ghargs.data == data
+    assert ghargs.kwargs == {'data': data}
+
+
 def test_equals():
     url = 'https://github.com/NCAR/xdev-bot'
     data = {'note': 'https://github.com/NCAR/xdev-bot/issues/11111'}
-    ghargs1 = GHArgs(url, data=data)
-    ghargs2 = GHArgs(url, data=data)
+    accept = 'application/vnd.github.inertia-preview+json'
+    ghargs1 = GHArgs(url, data=data, accept=accept)
+    ghargs2 = GHArgs(url, data=data, accept=accept)
     assert ghargs1 == ghargs2
--- xdev_bot/gidgethub.py
+++ xdev_bot/gidgethub.py
@@ -1,8 +1,9 @@
 class GHArgs(object):
 
-    def __init__(self, url, data=None):
+    def __init__(self, url, data=None, accept=None):
         self._url = url
         self._data = {} if data is None else data
+        self._accept = accept
 
     @property
     def url(self):
@@ -12,8 +13,19 @@ class GHArgs(object):
     def data(self):
         return self._data
 
+    @property
+    def accept(self):
+        return self._accept
+
+    @property
+    def kwargs(self):
+        kwds = {'data': self.data}
+        if self.accept is not None:
+            kwds['accept'] = self.accept
+        return kwds
+
     def __eq__(self, other):
         if not isinstance(other, GHArgs):
             return False
         else:
-            return self.url == other.url and self.data == other.data
+            return self.url == other.url and self.data == other.data and self.accept == other.accept
