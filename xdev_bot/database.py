f3a482ab52d06c71ee4510ad27bd9e9d30c87c5d
Kevin Paul <kpaul@ucar.edu>
2019-04-01 07:09:05 -0600

    Adding setitem, separating out save function, some cleanup

--- xdev_bot/database.py
+++ xdev_bot/database.py
@@ -10,7 +10,7 @@ class CardDB(object):
     def __init__(self, *cards, index=None, s3filename=None):
         if index is None:
             raise IndexError(f'Must set an unique index column name')
-        self._index = index
+        self._idxcol = index
         self._s3fn = s3filename
         self._df = self._read() if s3filename else pd.DataFrame()
         for card in cards:
@@ -19,33 +19,41 @@ class CardDB(object):
     def __len__(self):
         return len(self._df)
 
-    def __getitem__(self, item):
+    def __getitem__(self, index):
         if len(self._df) == 0:
             return None
-        elif self._index in self._df:
-            df = self._df[self._df[self._index] == item]
+        elif self._idxcol in self._df:
+            df = self._df[self._df[self._idxcol] == index]
             return df.iloc[0].to_dict() if len(df) == 1 else None
         else:
-            raise IndexError(f'Index {self._index} not found in database')
+            raise IndexError(f'Index {self._idxcol} not found in database')
+
+    def __setitem__(self, index, card):
+        _card = card.copy()
+        _card[self._idxcol] = index
+        if self[index] is None:
+            self._df = self._df.append(_card, ignore_index=True)
+        else:
+            card_cols = set(_card)
+            for col in card_cols.difference(self._df.columns):
+                self._df[col] = None
+            for col in card_cols:
+                i_row = self._df[self._df[self._idxcol] == index].index
+                j_col = self._df.columns.get_loc(col)
+                self._df.iloc[i_row, j_col] = _card[col]
 
     def add(self, card):
-        if self._index not in card:
-            raise IndexError(f'Index {self._index} not present in card {card}')
-        elif self[card[self._index]] is None:
-            self._df = self._df.append(card, ignore_index=True)
+        if self._idxcol not in card:
+            raise IndexError(f'Index {self._idxcol} not present in card {card}')
         else:
-            idx = self._df[self._df[self._index] == card[self._index]].index
-            self._df.iloc[idx] = [card[col] for col in self._df.columns]
-        if self._s3fn:
-            self._save()
+            index = card[self._idxcol]
+            self[index] = card
 
     def remove(self, card):
-        if self._index not in card or self[card[self._index]] is None:
+        if self._idxcol not in card or self[card[self._idxcol]] is None:
             raise KeyError(f'Card {card} not found in database')
-        idx = self._df[self._df[self._index] == card[self._index]].index
+        idx = self._df[self._df[self._idxcol] == card[self._idxcol]].index
         self._df = self._df.drop(idx).reset_index(drop=True)
-        if self._s3fn:
-            self._save()
 
     def _read(self):
         try:
@@ -58,10 +66,11 @@ class CardDB(object):
         except Exception:
             raise
 
-    def _save(self):
-        print(f'Saving database to file {self._s3fn} on Amazon S3')
-        with S3FS.open(self._s3fn, 'w') as f:
-            self._df.to_csv(f, index=False)
+    def save(self):
+        if self._s3fn:
+            print(f'Saving database to file {self._s3fn} on Amazon S3')
+            with S3FS.open(self._s3fn, 'w') as f:
+                self._df.to_csv(f, index=False)
 
 
 PROJECT_CARDS = CardDB(index='note', s3filename='xdev-bot/database.csv')
